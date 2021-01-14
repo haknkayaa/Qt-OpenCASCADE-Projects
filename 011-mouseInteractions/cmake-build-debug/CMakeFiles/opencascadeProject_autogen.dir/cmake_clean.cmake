df710d04ca89b90b0cd722b7b24bd8708b832b65
Hakan KAYA <mail@hakankaya.kim>
2021-01-14 15:13:26 +0300

    Delete 011-mouseInteractions/cmake-build-debug directory

--- 011-mouseInteractions/cmake-build-debug/CMakeFiles/opencascadeProject_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/opencascadeProject_autogen"
-  "opencascadeProject_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/opencascadeProject_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
