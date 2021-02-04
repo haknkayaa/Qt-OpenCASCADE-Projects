e6161b6d0fc5d910708156d53ef71bc758926c0a
haknkayaa <mail@hakankaya.kim>
2021-02-04 09:13:23 +0300

    update

---
+++ 012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/opencascadeProject_autogen/3YJK5W5UP7/qrc_resources.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/opencascadeProject_autogen/3YJK5W5UP7/qrc_resources.cpp.o"
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/opencascadeProject_autogen/3YJK5W5UP7/qrc_style.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/opencascadeProject_autogen/3YJK5W5UP7/qrc_style.cpp.o"
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/opencascadeProject_autogen/mocs_compilation.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/opencascadeProject_autogen/mocs_compilation.cpp.o"
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/src/MainWindow.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/src/MainWindow.cpp.o"
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/src/STEPProcessor.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/src/STEPProcessor.cpp.o"
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/src/Viewer.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/src/Viewer.cpp.o"
+  "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/src/main.cpp" "/media/hakan/250GB SSD/IRADETS/Projects/Qt-OpenCASCADE-Projects/012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  "QT_XML_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "opencascadeProject_autogen/include"
+  "/usr/lib/x86_64-linux-gnu/oce-0.17/../../../include/oce"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtXml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 012-UndoRedoFramework/cmake-build-debug/CMakeFiles/opencascadeProject_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencascadeProject_autogen"
+  "opencascadeProject_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/opencascadeProject_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
