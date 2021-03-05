2b639f4e6e827461e45195b5d04236f7c6cca719
sufuk <sufukguler@gmail.com>
2021-03-05 16:33:32 +0300

    Beautified

--- 023-ProjectWriterReader/src/ProjectCreator.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by sufuk on 26.02.2021.
-//
-
-#include "ProjectCreator.h"
-
-ProjectCreator::ProjectCreator(const QString &fileName) {
-
-}
--- 023-ProjectWriterReader/src/ProjectReader.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by sufuk on 26.02.2021.
-//
-
-#include "ProjectReader.h"
-
-ProjectReader::ProjectReader(const QString &fileName) {
-
-}
