9fdaff299650a43f3ab9434fc3dbdde1d4dd1e2d
sufuk <sufukguler@gmail.com>
2021-03-11 12:38:53 +0300

    Dynamic Translation Project

---
+++ 025-Dynamic-translation/translations/turkish.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="53"/>
+        <source>OpenCASCADE Window</source>
+        <translation>OpenCASCADE Penceresi</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="90"/>
+        <source>File</source>
+        <translation>Dosya</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="93"/>
+        <source>Import</source>
+        <translation>Aç</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="95"/>
+        <source>Add new file</source>
+        <translation>Yeni dosya ekle</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="98"/>
+        <source>Language</source>
+        <translation>Dil</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="101"/>
+        <location filename="../src/MainWindow.cpp" line="103"/>
+        <source>Turkish</source>
+        <translation>Türkçe</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="106"/>
+        <location filename="../src/MainWindow.cpp" line="108"/>
+        <source>English</source>
+        <translation>İngilizce</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="117"/>
+        <source>Ready</source>
+        <translation>Hazır</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="118"/>
+        <source>Not Selected File</source>
+        <translation>Dosya Seçilmedi</translation>
+    </message>
+    <message>
+        <source>Open File</source>
+        <translation type="vanished">Dosya Aç</translation>
+    </message>
+</context>
+<context>
+    <name>Viewer</name>
+    <message>
+        <location filename="../src/Viewer.cpp" line="121"/>
+        <source>Paint event</source>
+        <translation>Boyama Eventi</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="127"/>
+        <source>Resize Event</source>
+        <translation>Yeniden Boyutlandırma</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="155"/>
+        <source>Left mouse button clicked</source>
+        <translation>Sol buton tıklandı</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="160"/>
+        <source>Middle mouse button clicked</source>
+        <translation>Orta buton tıklandı</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="166"/>
+        <source>Right mouse button clicked</source>
+        <translation>Sağ buton tıklandı</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="175"/>
+        <source>Left mouse button released</source>
+        <translation>Sol buton bırakıldı</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="185"/>
+        <source>Middle mouse button released</source>
+        <translation>Orta buton bırakıldı</translation>
+    </message>
+    <message>
+        <location filename="../src/Viewer.cpp" line="189"/>
+        <source>Right mouse button released</source>
+        <translation>Sağ buton bırakıldı</translation>
+    </message>
+</context>
+</TS>
