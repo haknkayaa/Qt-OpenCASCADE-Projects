c8ef13510e45518e0fe60066c45f6580102d83e5
sufuk <sufukguler@gmail.com>
2021-03-30 10:14:24 +0300

    update

--- 026-LineCharts/src/MainWindow.cpp
+++ 026-LineCharts/src/MainWindow.cpp
@@ -294,6 +294,8 @@ void MainWindow::createMenuBar() {
     QAction *lineChartAction = new QAction("lineChart", this);
     connect(lineChartAction, &QAction::triggered, this, &MainWindow::slot_lineChart);
 
+    subMenu->addAction(lineChartAction);
+
     // Edit Menü
     QMenu *editMenu = new QMenu("Edit", this);
     menuBar->addMenu(editMenu);
@@ -671,7 +673,30 @@ void MainWindow::slot_fitAll() {
 
 void MainWindow::slot_lineChart() {
     qDebug() << "slot_lineChart";
+    QDialog *myDialog = new QDialog(this);
+    QVBoxLayout *dialogLayout = new QVBoxLayout(this);
+
     QLineSeries *series = new QLineSeries();
+    series->append(0, 6);
+    series->append(2, 4);
+    series->append(3, 8);
+    series->append(7, 4);
+    series->append(10, 5);
+    *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->setTitle("Simple line chart example");
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    dialogLayout->addWidget(chartView);
+
+    myDialog->setLayout(dialogLayout);
+    myDialog->show();
 }
 
 
