cfa9e4d7425404f8729c8315b518d230f4ba798b
sufuk <sufukguler@gmail.com>
2021-09-15 12:27:14 +0300

    Inintial

---
+++ 039-Qwt/src/ErrorBarPlotCurve.h
@@ -0,0 +1,22 @@
+//
+// Created by sufuk on 15.09.2021.
+//
+
+#ifndef PROJECT_ERRORBARPLOTCURVE_H
+#define PROJECT_ERRORBARPLOTCURVE_H
+#include <QtWidgets>
+#include <cmath>
+#include <QApplication>
+#include <QTimer>
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_point_data.h>
+
+class ErrorBarPlotCurve : public QwtPlotCurve{
+
+
+
+};
+
+
+#endif //PROJECT_ERRORBARPLOTCURVE_H
---
+++ 039-Qwt/src/MainWindow.cpp
@@ -0,0 +1,19 @@
+//
+// Created by sufuk on 15.09.2021.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+        QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
