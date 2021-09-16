789ea888cea984bc8669cbb55bd41c4f13ad689c
haknkayaa <mail@hakankaya.kim>
2021-09-16 13:07:24 +0300

    updated

---
+++ 038-qmlOpenCascade/cmake-build-debug-coverage/CMakeFiles/QMLProject.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QMLProject_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QMLProject_autogen.dir/ParseCache.txt"
+  "QMLProject_autogen"
+  "CMakeFiles/QMLProject.dir/QMLProject_autogen/6WJNPILU4A/qrc_qml.cpp.o"
+  "CMakeFiles/QMLProject.dir/src/main.cpp.o"
+  "CMakeFiles/QMLProject.dir/src/opencascade.cpp.o"
+  "QMLProject"
+  "QMLProject.pdb"
+  "QMLProject_autogen/6WJNPILU4A/qrc_qml.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QMLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
