1634d6d7cc5f8e5f0e69bba68aeeb9205f872fe3
hakan <mail@hakankaya.kim>
2021-09-27 13:33:50 +0300

    updated

--- 021-ribbonTabToolbar/cmake-build-debug/CMakeFiles/ribbonTabToolbar_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ribbonTabToolbar_autogen"
-  "ribbonTabToolbar_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ribbonTabToolbar_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- 038-qmlOpenCascade/cmake-build-debug/CMakeFiles/QMLProject_autogen.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/QMLProject_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QMLProject_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
